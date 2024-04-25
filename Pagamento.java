f9778d6c4c81e1bf0cb2cb711070b3b77e5b74b3
David Guilherme B. Denunci <118739281+davidgd11@users.noreply.github.com>
2024-04-25 08:43:13 -0300

    Update Pagamento.java

--- Pagamento.java
+++ Pagamento.java
@@ -2,58 +2,58 @@ package br.com.fiap.main;
 
 import javax.swing.JOptionPane;
 
+import br.com.fiap.bean.Funcionario;
 import br.com.fiap.bean.Vendedor;
 import br.com.fiap.bean.VigiaNoturno;
 
 public class Pagamento {
 
 	public static void main(String[] args) {
-		
-		String aux, nome, escolha = "sim";
-		float salario, valorHoraTrabalho, comissao, adicionalNoturno;
-		int opcao;
-		
-		while(escolha.equalsIgnoreCase("sim")) {
+		String aux, nome, exibe, escolha = "sim";
+		float salario, valorHora;
+		while (escolha.equalsIgnoreCase("sim")) { 
 			try {
-				aux = JOptionPane.showInputDialog("Qual funcionário você quer calcular o salário?"
-						+ "\n(1)Vendedor \n(2)Vigia noturno");
-				opcao = Integer.parseInt(aux);
-				switch(opcao) {
-				case 1: 
-					Vendedor vend;
-					aux = JOptionPane.showInputDialog("Qual é o nome do vendedor?");
-					nome = aux;
-					aux = JOptionPane.showInputDialog("Qual é o valor da hora do vendedor?");
-					valorHoraTrabalho = Float.parseFloat(aux);
-					aux = JOptionPane.showInputDialog("Qual será a comissão do(a) " + nome + "?");
-					comissao = Float.parseFloat(aux);
-					vend = new Vendedor(nome, valorHoraTrabalho, comissao);
-					salario = vend.calculaSalario();
-					JOptionPane.showMessageDialog(null, nome + " teve uma comissão de: R$ " 
-							+ comissao + "\nTotalizando um salário de: R$ " + salario);
+				nome = JOptionPane.showInputDialog("Digite seu nome"); 
+
+				aux = JOptionPane.showInputDialog("Digite valor da hora de trabalho"); 
+				valorHora = Float.parseFloat(aux);
+				
+				aux = JOptionPane.showInputDialog("Qual o tipo de funcionario?" 
+						+ "\n(1) Normal \n(2) Vigia noturno \n(3) Vendedor");
+				int opcao = Integer.parseInt(aux);
+				
+				switch (opcao) {
+				case 1:
+					Funcionario normal = new Funcionario(nome, valorHora); 
+					salario = normal.calculaSalario(); 
+					exibe = "Exibindo os dados: \nNome: " + normal.getNome()
+							+ "\nSalario: R$ " + salario;
 					break;
 				case 2:
-					VigiaNoturno vigia;
-					aux = JOptionPane.showInputDialog("Qual é o nome do vigia?");
-					nome = aux;
-					aux = JOptionPane.showInputDialog("Qual é o valor da hora do vigia?");
-					valorHoraTrabalho = Float.parseFloat(aux);
-					aux = JOptionPane.showInputDialog("Qual é o adicional do vigia noturno? ");
-					adicionalNoturno = Float.parseFloat(aux);
-					vigia = new VigiaNoturno(nome, valorHoraTrabalho, adicionalNoturno);
-					salario = vigia.calculaSalario();
-					JOptionPane.showMessageDialog(null, nome + " teve um adicional de: R$ " 
-							+ adicionalNoturno + "\nTotalizando um salário de: R$ " + salario);
+					aux = JOptionPane.showInputDialog("Qual valor do adicional noturno mensal?");
+					float adicional = Float.parseFloat(aux);
+					VigiaNoturno vigia = new VigiaNoturno(nome, valorHora, adicional); 
+					salario = vigia.calculaSalario(); 
+					exibe = "Exibindo os dados: \nNome: " + vigia.getNome()
+							+ "\nSalario: R$ " + salario;
+					break;
+				case 3:
+					aux = JOptionPane.showInputDialog("Qual a porcetagem da comissao?"); 
+					float comissao = Float.parseFloat(aux);
+					Vendedor vend = new Vendedor(nome, valorHora, comissao); 
+					salario = vend.calculaSalario(); 
+					exibe = "Exibindo os dados: \nNome: " + vend.getNome()
+							+ "\nSalario: R$ " + salario;
 					break;
 				default:
-					throw new IllegalArgumentException("Valor incorreto");
+					throw new Exception("Escolha incorreta!");
 				}
+				JOptionPane.showMessageDialog(null, exibe);
 				escolha = JOptionPane.showInputDialog("Deseja continuar?");
 			} catch (Exception e) {
 				JOptionPane.showMessageDialog(null, e.getMessage());
 			}
 		}
-		JOptionPane.showMessageDialog(null, "Fim do programa obrigado!");
+		JOptionPane.showMessageDialog(null, "Fim de programa. Volte sempre!");
 	}
-
 }
